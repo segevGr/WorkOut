c961364a56aff98f842b543fb6b151552dc1e26c
segevGr <segev126@gmail.com>
2024-01-25 15:19:11 +0200

    add db connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,21 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log(`Database connected successfully`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err.message);
+  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
