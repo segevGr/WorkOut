07b32d5de2de8766fe9332e7a5b06498deba42d4
segevGr <segev126@gmail.com>
2024-01-29 14:51:15 +0200

    add some APIs to muscles

--- Backend/controllers/Muscles.js
+++ Backend/controllers/Muscles.js
@@ -1,31 +1,62 @@
 const Muscles = require("../models/Muscles");
 
-// exports.getUserById = (req, res) => {
-//   const id = req.params.id * 1;
-//   const user = users.find((item) => item.id === id);
-//   !user
-//     ? res.status(404).json({
-//         status: "Fail",
-//         message: "User not found!",
-//       })
-//     : res.status(200).json({
-//         status: "Success",
-//         data: { user },
-//       });
-// };
-
 exports.getAllMuscles = async (req, res) => {
   try {
     const muscles = await Muscles.find();
     res.status(200).json({
-      status: "Success",
+      status: "success",
       results: muscles.length,
       data: { muscles },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(500).json({
       status: "fail",
       message: err,
+    });
+  }
+};
+
+exports.getMuscleById = async (req, res) => {
+  try {
+    const muscle = await Muscles.findById(req.params.id);
+    if (!muscle) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Muscle not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      results: muscle.length,
+      data: { muscle },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getMuscleByName = async (req, res) => {
+  try {
+    const muscle = await Muscles.findOne({ muscleName: req.params.name });
+    if (!muscle) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Muscle not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      results: 1,
+      data: { muscle },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
     });
   }
 };
@@ -41,6 +72,54 @@ exports.createMuscle = async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.updateMuscle = async (req, res) => {
+  try {
+    const muscle = await Muscles.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!muscle) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Muscle not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        muscle,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.deleteMuscle = async (req, res) => {
+  try {
+    await Muscles.findByIdAndDelete(req.params.id);
+    if (!result) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Muscle not found",
+      });
+    }
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(500).json({
       status: "fail",
       message: err,
     });
--- Backend/routes/Muscles.js
+++ Backend/routes/Muscles.js
@@ -7,6 +7,12 @@ router
   .route("/")
   .get(MusclesController.getAllMuscles)
   .post(MusclesController.createMuscle);
-// router.route("/:id").get(MusclesController.getUserById);
+
+router
+  .route("/:id")
+  .get(MusclesController.getMuscleById)
+  .patch(MusclesController.updateMuscle)
+  .delete(MusclesController.deleteMuscle);
+router.route("/name/:name").get(MusclesController.getMuscleByName);
 
 module.exports = router;
