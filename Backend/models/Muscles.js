93273cebaa326bd3d0c28bc1b02ef8586debee18
segevGr <segev126@gmail.com>
2024-01-31 17:47:42 +0200

    add image to schame

--- Backend/models/Muscles.js
+++ Backend/models/Muscles.js
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose");
 
-const muscles = new mongoose.Schema({
-  muscleName: {
-    type: String,
-    required: [true, "Muscle name must have a name"],
-    unique: true,
-    trim: true,
+const muscles = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Muscle must to have a name"],
+      unique: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: [true, "Muscle must to have an image"],
+      unique: true,
+      trim: true,
+    },
   },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+muscles.virtual("VirtualTest").get(function () {
+  return this.name;
 });
 
 const Muscles = mongoose.model("Muscles", muscles);
