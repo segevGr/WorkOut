5590d684bb028e2e80f088d0d974c1879c159e33
segevGr <segev126@gmail.com>
2024-02-19 15:50:55 +0200

    remove notes

--- Backend/controllers/WorkoutsController.js
+++ Backend/controllers/WorkoutsController.js
@@ -10,38 +10,6 @@ exports.getWorkoutsById = factory.getOne(Workouts, "workout", {
 });
 exports.createWorkout = factory.createOne(Workouts, "workout");
 
-// exports.updateExerciseFromWorkout = catchAsync(async (req, res, next) => {
-//   const filters = {
-//     _id: req.params.workoutId,
-//     "exercises._id": req.body.exerciseId,
-//   };
-
-//   const { sets, notes } = req.body;
-//   const update = {};
-//   if (sets !== undefined) {
-//     update["$set"] = { "exercises.$.sets": sets };
-//   }
-//   if (notes !== undefined) {
-//     update["$set"]["exercises.$.notes"] = notes;
-//   }
-
-//   const workout = await Workouts.findByIdAndUpdate(filters, update, {
-//     new: true,
-//     runValidators: true,
-//   });
-//   if (!workout) {
-//     return next(
-//       new AppError("No workout or exercise found with that IDs", 404)
-//     );
-//   }
-//   res.status(200).json({
-//     status: "success",
-//     data: {
-//       workout,
-//     },
-//   });
-// });
-
 exports.updateExerciseFromWorkout = catchAsync(async (req, res, next) => {
   const workoutId = req.params.workoutId;
   const exerciseId = req.params.exerciseId;
