e940257532f251acb879632af23eb4fc666ebc19
segevGr <segev126@gmail.com>
2024-02-19 11:04:05 +0200

    allow nasted route with user&workout

--- Backend/controllers/HandlerFactory.js
+++ Backend/controllers/HandlerFactory.js
@@ -63,7 +63,10 @@ exports.getOne = (Model, dataName, populateOptions) =>
 
 exports.getAll = (Model, dataName) =>
   catchAsync(async (req, res, next) => {
-    const features = new APIFeatures(Model.find(), req.query)
+    let filter = {};
+    if (req.params.userId) filter = { user: req.params.userId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
--- Backend/controllers/WorkoutsController.js
+++ Backend/controllers/WorkoutsController.js
@@ -3,24 +3,6 @@ const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
 const factory = require("./HandlerFactory");
 
-exports.getMuscleNameById = catchAsync(async (muscleID) => {
-  const muscle = await Workouts.findById(muscleID);
-  const result = muscle ? muscle.muscleName : "Muscle not found";
-  return result;
-});
-
-exports.getMuscleByName = catchAsync(async (req, res, next) => {
-  const muscle = await Workouts.findOne({ muscleName: req.params.name });
-  if (!muscle) {
-    return next(new AppError("No muscle found with that ID", 404));
-  }
-  res.status(200).json({
-    status: "Success",
-    results: 1,
-    data: { muscle },
-  });
-});
-
 exports.getAllWorkouts = factory.getAll(Workouts, "workouts");
 exports.getMuscleById = factory.getOne(Workouts, "workouts");
 exports.createWorkout = factory.createOne(Workouts, "workouts");
