723a0b1f43df415ca631c039ce708fd8d4fb6d8b
segevGr <segev126@gmail.com>
2024-04-24 16:54:26 +0300

    increase the number of allowed requests

--- Backend/app.js
+++ Backend/app.js
@@ -23,7 +23,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again later.",
 });
